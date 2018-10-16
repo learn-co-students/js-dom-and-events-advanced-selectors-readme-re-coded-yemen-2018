5d87e52a030e3f5cfdde5f450632648e7cddcb67
Farida <faridah.absi@gmail.com>
2018-10-16 10:48:08 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+      return $('p');
+    }
+    
+   function lastImageSelector(){
+     return $('.pics #pic-list li:last img');
+   }
+   
+   function ninjaBabySelector(){
+     return $('#baby-ninja');
+   }
+   
+   function divSelector(){
+     return $('.pics');
+   }
+   function firstListItem(){
+     return $('.pics #pic-list li:first-child');
+   }
+   
+   
